9e70b1d8183e32d07d3097c09b6aa75f18df8683
Huong <admin@Apples-MacBook-Pro.local>
2019-02-24 11:23:56 +0700

    class Tinh

--- index.js
+++ index.js
@@ -25,14 +25,33 @@ app.get('/a',(req,res)=>{
     res.sendFile(__dirname+'./views/a.html');
 });
 
+app.get('/:so_a/:pheptinh/:so_b',(req,res)=>{
+    const {so_a , pheptinh, so_b} = req.params
+    const t = new Tinh(so_a,so_b,pheptinh)
+    res.send(t.getResult())
+})
+
 
 class Tinh{
-    constructor(){
-
+    constructor(number_a,number_b,tenpheptinh){
+        this.number_a = number_a;
+        this.number_b = number_b;
+        this.tenpheptinh = tenpheptinh
     }
-    
+    get pheptinh(){
+        if(this.tenpheptinh==='cong') return '+'
+        else if(this.tenpheptinh==='tru') return '-'
+        else if(this.tenpheptinh==='nhan') return '*'
+        return '/'
+    }
+    getResult(){
+        // 4 + 5 = 9
+        const string = `${this.number_a} ${this.pheptinh} ${this.number_b}`
+        return `${string} = ${eval(string)}` 
+    }
 }
-
+// const t = new Tinh(2,3,'+')
+// console.log(t.getResult())
 
 const port = process.env.PORT || 3000
 app.listen(port,()=>{
