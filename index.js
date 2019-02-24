042fe00fc65757417d8f9f2805d994eb901ffe0e
Huong <admin@Apples-MacBook-Pro.local>
2019-02-24 10:57:47 +0700

    app.get

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-/**
- * npm install yarn -g
- * yarn -v
- * 
- * yarn init -y
- * yarn add express ejs
- */
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+
+
+app.get('/',(request,response)=>{
+    response.send({
+        name:'Teo',
+        height: 180
+    })
+});
+app.get('/user/:id',(req,res)=>{
+    const id = +req.params.id
+    const type = typeof id
+    res.send({
+        id,
+        type
+    })
+});
+
+app.get('/a',(req,res)=>{
+    // res.sendStatus(404)
+    // console.log(__dirname);
+    res.sendFile(__dirname+'./views/a.html');
+});
+
+
+class Tinh{
+    constructor(){
+
+    }
+    
+}
+
+
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log('Server started on port '+port)
+})
+
+
+
